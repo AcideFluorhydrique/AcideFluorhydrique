c62be6278b807770dea53361137448700daccb28
Ariel Geo Wang <139669368+AcideFluorhydrique@users.noreply.github.com>
2024-12-11 20:32:16 +0800

    Create getWeChat.c

---
+++ getWeChat.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char num2char[] = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z', '_'
+};
