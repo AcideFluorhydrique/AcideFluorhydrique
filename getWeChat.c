ae47537aa359046947db1fe86d861850859a420b
Ariel Geo Wang <139669368+AcideFluorhydrique@users.noreply.github.com>
2024-12-12 20:07:03 +0800

    Update getWeChat.c

--- getWeChat.c
+++ getWeChat.c
@@ -7,3 +7,29 @@ char num2char[] = {
     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
     'u', 'v', 'w', 'x', 'y', 'z', '_'
 };
+
+void int2str(char *input) {
+    char cleaned_input[1000];
+    int cleaned_len = 0;
+    for (int i = 0; i < strlen(input); i++) {
+        if (input[i] != ' ') {
+            cleaned_input[cleaned_len++] = input[i];
+        }
+    }
+    cleaned_input[cleaned_len] = '\0';
+    char result[500];
+    int result_len = 0;
+    for (int i = 0; i < cleaned_len; i += 2) {
+        char num_str[3] = { cleaned_input[i], cleaned_input[i + 1], '\0' };
+        int num = atoi(num_str);
+
+        if (num >= 1 && num <= 27) {
+            result[result_len++] = num2char[num - 1];
+        } else {
+            printf("Error!\n");
+            return;
+        }
+    }
+    result[result_len] = '\0';
+    printf("My WeChat ID is: %s\n", result);
+}
